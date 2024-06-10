967b59004b1e614dff3cb16f7e71e4d97578ebe2
Paolo Arroyo <paolo@LEGION-LT>
2024-06-11 01:22:16 +0800

    feat: connect frontend to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
