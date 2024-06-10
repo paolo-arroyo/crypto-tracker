e95920e8be81710d2771ae6db9b344d142380e77
Paolo Arroyo <paolo@LEGION-LT>
2024-06-11 02:39:04 +0800

    chore: improve error message

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -8,9 +8,9 @@ const ErrorPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">Oops! Something went wrong.</h1>
-      <p className="text-lg mb-8">Please try again later.</p>
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-3xl font-bold my-6">Oops! Something went wrong.</h1>
+      <p className="text-lg mb-8"> Please try again in a minute. </p>
       <button
         className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleBackClick}
