bab458247cfdd41002be939d36b695c8e01197aa
Paolo Arroyo <paolo@LEGION-LT>
2024-06-11 01:23:41 +0800

    feat: add cache with node-cache

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
+import NodeCache from 'node-cache';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const cache = new NodeCache({ stdTTL: 60 })
 
 app.use(cors());
+
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
